6ade2c090452bc21b15371189fd3fcd66cc0bb9a
me10 <medisadido@gmail.com>
2025-02-14 11:22:23 +0100

    appleme

--- app/routes/main/utils.py
+++ app/routes/main/utils.py
@@ -3,37 +3,30 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from PIL import Image, UnidentifiedImageError
+import os
+from flask import current_app
 
 MODEL_PATH = 'models/waste_classifier_model.h5'
-CLASS_NAMES = ["électroniques", "automobiles", "de batterie", "plastiques", "Ampoules", "métalliques", "verre", "de papier", "organiques"]
+CLASS_NAMES = ["électroniques", "automobiles", "batterie", "plastiques", "Ampoules", "métalliques", "organiques", "de papier", "verre"]
 
 # Charger le modèle TensorFlow
 model = tf.keras.models.load_model(MODEL_PATH)
 
 def preprocess_image(file, filename):
     """Traite l'image (chargement, redimensionnement et normalisation)"""
-    file_bytes = np.frombuffer(file.read(), np.uint8)
-    print(file_bytes[:100])  # Affiche les 100 premiers octets du fichier
-
     try:
-        # Tentative de décodage avec OpenCV
-        image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-        if image is None:
-            raise ValueError("Échec de la décodification de l'image avec OpenCV")
-        
-        image = cv2.resize(image, (224, 224))  # Redimensionner pour correspondre au modèle
-        image = image / 255.0  # Normaliser l'image
+        # Prétraitement de l'image
+        image = cv2.imread(image_path)
+        image = cv2.resize(image, (224, 224))  # Redimensionner selon les besoins du modèle
+        image = image / 255.0  # Normaliser
         image = np.expand_dims(image, axis=0)  # Ajouter une dimension pour le batch
-
     except Exception as e:
         print(f"Erreur avec OpenCV : {e}, tentative avec PIL")
         
         # Tentative avec PIL si OpenCV échoue
         try:
-            image_path = '/home/juste/appython/recycy/app/static/' + filename
-
+            image_path = os.path.join(current_app.config['UPLOAD_FOLDER']) + filename
             image = load_img(image_path, target_size=(224, 224))
-
             image = img_to_array(image)
             image = image / 255.0
             image = np.expand_dims(image, axis=0)  # Ajouter la dimension du batch
